26b486bd0047f9a900aadadfa13d204a89de8c65
denise030261 <83278071+denise030261@users.noreply.github.com>
2024-03-04 22:56:35 +0900

    소트인사이드 문제 풀이
    백준 1427번 소트인사이드 문제 풀이 (프로젝트 열림 오류 해결)

---
+++ BaekjoonSilver/소트인사이드.cpp
@@ -0,0 +1,33 @@
+@ - 1, 32 + 0, 0 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(int a, int b)
+{
+	return a > b;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	string str;
+	vector<int> answer;
+
+	cin >> str;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		answer.push_back(str[i] - '0');
+	}
+
+	sort(answer.begin(), answer.end(), compare);
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		cout << answer[i];
+	}
+}
