a298633d40fa4ca5211292771a5ed942c72e89bc
denise030261 <83278071+denise030261@users.noreply.github.com>
2024-03-06 16:12:41 +0900

    곱셈 문제 풀이
    백준 1629번 곱셈 문제 풀이

---
+++ BaekjoonSilver/곱셈.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+long long A, B, C, k;
+
+long long power(long long b) {
+	if (b == 0) return 1;
+	if (b == 1) return A % C;
+
+	k = power(b / 2) % C;
+	if (b % 2 == 0) return k * k % C;
+	return k * k % C * A % C;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> A >> B >> C;
+
+	cout << power(B);
+}
