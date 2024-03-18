49a66a135af2c65d23fc75667fd0a21a869f847e
denise030261 <83278071+denise030261@users.noreply.github.com>
2024-03-18 23:54:41 +0900

    백준 수 정렬하기 4 문제 풀이
    백준 11931번 수 정렬하기 4 문제 풀이

---
+++ BaekjoonSilver/수_정렬하기4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(int a, int b)
+{
+	return a > b;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	vector<int> v;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << v[i] << '\n';
+	}
+}
